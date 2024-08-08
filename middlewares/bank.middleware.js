80fa01418b73b60a20d18d69d3c020ca3d0a35e4
Rushikesh Acharya <rushikeshacharya09@gmail.com>
2024-08-08 23:52:32 +0530

    update: bank middleware

--- middlewares/bank.middleware.js
+++ middlewares/bank.middleware.js
@@ -1,6 +1,9 @@
 const validateDepositInput = (req, res, next) => {
-  if (!req.body.accountNumber || !req.body.amount) {
+  if (!req.body.accountNumber || !req.body.amount ) {
     return res.status(400).json({ message: "Invalid Inputs" });
+  }
+  if (req.body.amount < 0) {
+    return res.status(400).json({ message: "Invalid Amount" });
   }
   return next();
 };
